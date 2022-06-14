eb8b127267c7dff44963bfe97e75ada4fb6d68c6
malken21 <Malken_Marumasa@outlook.jp>
2022-06-15 01:26:39 +0900

    Add files via upload

---
+++ java/marumasa/auto_message/Config.java
@@ -0,0 +1,20 @@
+package marumasa.auto_message;
+
+import org.bukkit.configuration.file.FileConfiguration;
+
+import java.util.List;
+
+public class Config {
+
+    public final List<String> Messages;
+    public final long tick;
+    public final boolean AllSendMessage;
+
+    public Config(Minecraft plugin) {
+        plugin.saveDefaultConfig();
+        FileConfiguration config = plugin.getConfig();
+        Messages = config.getStringList("Messages");
+        tick = config.getLong("tick", 6000L);
+        AllSendMessage = config.getBoolean("AllSendMessage",true);
+    }
+}
---
+++ java/marumasa/auto_message/Minecraft.java
@@ -0,0 +1,18 @@
+package marumasa.auto_message;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Minecraft extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        Config config = new Config(this);
+        getLogger().info(String.valueOf(config.Messages));
+        getLogger().info(String.valueOf(config.tick));
+        new SendMessage(config).runTaskTimer(this, 0L, config.tick);
+    }
+
+    @Override
+    public void onDisable() {
+    }
+}
